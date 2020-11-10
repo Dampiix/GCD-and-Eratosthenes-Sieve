4f46dd0eb379d4d140f76c79f367808393dec188
Dampiix <56698857+Dampiix@users.noreply.github.com>
2020-11-10 10:58:45 +0100

    Added: -> read new Properties     - prename     - surname     - matr nr

--- app/src/main/java/MyJavaProg/ReadProperties.java
+++ app/src/main/java/MyJavaProg/ReadProperties.java
@@ -3,6 +3,7 @@ package MyJavaProg;
 public class ReadProperties {
 
     public void readProperties(){
+        //
         App.Vorname = " " + Console.readStringFromStdIn("Enter prename: ");
         App.Name    = " " + Console.readStringFromStdIn("Enter surname: ");
         App.MatrNr  = " " + Console.readStringFromStdIn("Enter matriculation number: ");
